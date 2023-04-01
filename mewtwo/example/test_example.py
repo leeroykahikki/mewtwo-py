37030da2e8de93d9510c20fb3677357461800c2c
Штирбу Эдуард <60253601+leeroykahikki@users.noreply.github.com>
2023-04-01 14:11:33 +0300

    Изменён примерный образец для работы с библиотекой

--- mewtwo/example/test_example.py
+++ mewtwo/example/test_example.py
@@ -1,6 +1,6 @@
 import unittest
-from example import division_example, minus_example, plus_example, multiplication_example
-
+from unittest.suite import TestSuite
+from mewtwo.example.example import plus_example, minus_example, division_example, multiplication_example
 
 class MyTestCase(unittest.TestCase):
     def test_plus(self):
@@ -16,5 +16,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(division_example(6, 2), 3)
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    suite = TestSuite()
+    tests = unittest.TestLoader()
+
+    suite.addTest(tests.loadTestsFromTestCase(MyTestCase))
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
